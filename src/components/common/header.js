abb7e12ff6c8ffebabd747f27085eb498d88c944
MiGlez <miglez90290995@gmail.com>
2017-08-11 16:06:20 -0500

    Se implementó react-router :octocat:

--- src/components/common/header.js
+++ src/components/common/header.js
@@ -7,11 +7,13 @@ var Header = React.createClass({
     return (
       <nav className="navbar navbar-default">
         <div className="container-fluid">
-          <a href="/" className="navbar-brand">
-            <img src="images/fixter.png" alt="pluralsight" width="140" height="35"/>
-          </a>
+          <div className="navbar-header">
+            <a className="navbar-brand" href="/">
+              <img src="images/fixter.png" alt="pluralsight" width="140" height="35"/>
+            </a>
+          </div>
           <ul className="nav navbar-nav">
-            <li><a href="/">Home</a></li>
+            <li><a className="active" href="/">Home</a></li>
             <li><a href="/#authors">Authors</a></li>
             <li><a href="/#about">About</a></li>
           </ul>
--- src/components/homePage.js
+++ src/components/homePage.js
@@ -5,10 +5,13 @@ var React = require('react');
 var Home = React.createClass({
   render: function () {
     return (
-      <div className="jumbotron">
-        <h1>Fixter Administration</h1>
-        <p>React, React Router, and Flux for ultra responsive web apps.</p>
+      <div className="container-fluid">
+        <div className="jumbotron">
+          <h1>Fixter Administration</h1>
+          <p>React, React Router, and Flux for ultra responsive web apps.</p>
+        </div>
       </div>
+
     );
   }
 });
