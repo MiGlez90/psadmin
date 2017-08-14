5fa4caf29220d7ccd8c6f769f8d8b65464e0d6c8
MiGlez <miglez90290995@gmail.com>
2017-08-14 15:54:24 -0500

    se agregaron transiciones :octocat:

--- dist/scripts/bundle.js
+++ dist/scripts/bundle.js
@@ -49236,6 +49236,21 @@ module.exports = {
 var React = require('react');
 
 var About = React.createClass({displayName: "About",
+  statics: {
+    willTransitionTo: function( transition, params, query, callback) {
+      if (!confirm('Are you sure you wanna read a page that\'s this boring?')) {
+        transition.about();
+      } else {
+        callback();
+      }
+    },
+
+    willTransitionFrom: function( transition, component) {
+      if (!confirm('Are you sure you wanna leave a page that\'s this exciting?')) {
+        transition.about();
+      }
+    }
+  },
   render: function () {
     return (
       React.createElement("div", null, 
